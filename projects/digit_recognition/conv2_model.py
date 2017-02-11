a72f95273d713b52d0275f4efac7d794829313eb
Vadzim Dabravolski <vadzim_dabravolski@epam.com>
2017-02-11 13:47:19 -0500

    Problem 1 solved

--- projects/digit_recognition/conv2_model.py
+++ projects/digit_recognition/conv2_model.py
@@ -19,6 +19,7 @@ from keras.models import Model
 from keras.utils import np_utils
 from keras import backend as K
 from mnist_data_preps import generate_sequences
+from matplotlib import pyplot as plt
 
 batch_size = 128
 nb_classes = 11  # include 10 digits and "-1" which designates empty space in digit sequence.
@@ -33,24 +34,24 @@ nb_filters = 32
 pool_size = (2, 2)
 # convolution kernel size
 kernel_size = (3, 3)
+weights_path = "tmp/vanilla_cnn_weights.hdf5"
 
 # the data, shuffled and split between train and test sets
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-X_train, Y_train, Y_train_length = generate_sequences(train_images, train_labels, out_size=np.shape(train_images)[0],
+X_train_orig, Y_train, Y_train_length = generate_sequences(train_images, train_labels, out_size=np.shape(train_images)[0],
                                                       max_length=sequence_length)
-X_test, Y_test, Y_test_length = generate_sequences(test_images, test_labels, out_size=np.shape(test_images)[0],
+X_test_orig, Y_test, Y_test_length = generate_sequences(test_images, test_labels, out_size=np.shape(test_images)[0],
                                                    max_length=sequence_length)
 
 if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    X_train = X_train_orig.reshape(X_train_orig.shape[0], 1, img_rows, img_cols)
+    X_test = X_test_orig.reshape(X_test_orig.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    X_train = X_train_orig.reshape(X_train_orig.shape[0], img_rows, img_cols, 1)
+    X_test = X_test_orig.reshape(X_test_orig.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
-
 
 # Classifiers' inputs and targets
 X_train = X_train.astype('float32')
@@ -80,60 +81,110 @@ forth_cls_test = Y_test[:, 3, :]
 fifth_cls_train = Y_train[:, 4, :]
 fifth_cls_test = Y_test[:, 4, :]
 
-
-# Shared feature layers which will be used for multiple classifiers.
-main_input = Input(shape=input_shape, dtype='float32', name='main_input')
-shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                       border_mode='valid', input_shape=input_shape)(main_input)
-shared = Activation('relu')(shared)
-shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1])(shared)
-shared = Activation('relu')(shared)
-shared = MaxPooling2D(pool_size=pool_size)(shared)
-shared = Dropout(0.25)(shared)
-shared = Flatten()(shared)
-shared = Dense(128)(shared)
-shared = Activation('relu')(shared)
-shared = Dropout(0.5)(shared)
-
-# 6 different classifiers.
-# 1. Length classifier
-length_cls = Dense((sequence_length + 1))(shared)  # to account for sequence length + 1
-length_cls = Activation('softmax', name="length")(length_cls)
-
-# 2. First digit classifier
-first_cls = Dense(nb_classes)(shared)
-first_cls = Activation('softmax', name="first_digit")(first_cls)
-
-# 3. Second digit classifier
-second_cls = Dense(nb_classes)(shared)
-second_cls = Activation('softmax', name="second_digit")(second_cls)
-
-# 4. Third digit classifier
-third_cls = Dense(nb_classes)(shared)
-third_cls = Activation('softmax', name="third_digit")(third_cls)
-
-# 5. Forth digit classifier
-forth_cls = Dense(nb_classes)(shared)
-forth_cls = Activation('softmax', name="forth_digit")(forth_cls)
-
-# 6. Fifth digit classifier
-fifth_cls = Dense(nb_classes)(shared)
-fifth_cls = Activation('softmax', name="fifth_digit")(fifth_cls)
+train_targets = [length_cls_train, first_cls_train, second_cls_train, third_cls_train, forth_cls_train, fifth_cls_train]
+test_targets = [length_cls_test, first_cls_test, second_cls_test, third_cls_test, forth_cls_test, fifth_cls_test]
+cls_weights = [1., 1., 1., 1., 1., 1.]
 
 
-# model compilation and training
-model = Model(input=[main_input], output=[length_cls, first_cls, second_cls, third_cls, forth_cls, fifth_cls])
+def create_vanilla_CNN(cls_weights):
+    # Shared feature layers which will be used for multiple classifiers.
+    main_input = Input(shape=input_shape, dtype='float32', name='main_input')
+    shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                           border_mode='valid', input_shape=input_shape)(main_input)
+    shared = Activation('relu')(shared)
+    shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1])(shared)
+    shared = Activation('relu')(shared)
+    shared = MaxPooling2D(pool_size=pool_size)(shared)
+    shared = Dropout(0.25)(shared)
+    shared = Flatten()(shared)
+    shared = Dense(128)(shared)
+    shared = Activation('relu')(shared)
+    shared = Dropout(0.5)(shared)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'], loss_weights=[1., 1., 1., 1., 1., 1.])
+    # 6 different classifiers.
+    # 1. Length classifier
+    length_cls = Dense((sequence_length + 1))(shared)  # to account for sequence length + 1
+    length_cls = Activation('softmax', name="length")(length_cls)
 
-checkpointer = ModelCheckpoint(filepath="/tmp/weights.hdf5", verbose=1, save_best_only=True)
+    # 2. First digit classifier
+    first_cls = Dense(nb_classes)(shared)
+    first_cls = Activation('softmax', name="first_digit")(first_cls)
 
-model.fit([X_train], [length_cls_train, first_cls_train, second_cls_train, third_cls_train, forth_cls_train,
-                      fifth_cls_train], batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, validation_data=([X_test], [length_cls_test,first_cls_test, second_cls_test,
-                                                 third_cls_test, forth_cls_test, fifth_cls_test]), callbacks=[checkpointer])
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+    # 3. Second digit classifier
+    second_cls = Dense(nb_classes)(shared)
+    second_cls = Activation('softmax', name="second_digit")(second_cls)
+
+    # 4. Third digit classifier
+    third_cls = Dense(nb_classes)(shared)
+    third_cls = Activation('softmax', name="third_digit")(third_cls)
+
+    # 5. Forth digit classifier
+    forth_cls = Dense(nb_classes)(shared)
+    forth_cls = Activation('softmax', name="forth_digit")(forth_cls)
+
+    # 6. Fifth digit classifier
+    fifth_cls = Dense(nb_classes)(shared)
+    fifth_cls = Activation('softmax', name="fifth_digit")(fifth_cls)
+
+    # model compilation and training
+    model = Model(input=[main_input], output=[length_cls, first_cls, second_cls, third_cls, forth_cls, fifth_cls])
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'], loss_weights=cls_weights)
+
+    return model
+
+
+def train_model(model, train_inputs, train_targets, test_inputs, test_targets, use_trained=False):
+    if use_trained:
+        model = model
+        model.load_weights(weights_path)
+    else:
+        checkpointer = ModelCheckpoint(filepath=weights_path, verbose=1, save_best_only=True)  # TODO: add relative path
+        model.fit([train_inputs], train_targets, batch_size=batch_size, nb_epoch=nb_epoch,
+                  verbose=1, validation_data=([test_inputs], test_targets), callbacks=[checkpointer])
+
+    return model
+
+
+def evaluate_model(model, eval_inputs, eval_targets, verbose=1):
+    score = model.evaluate(eval_inputs, eval_targets, verbose=verbose)
+    print('Length accuracy:', score[7])
+    print('First digit accuracy:', score[8])
+    print('Second digit accuracy:', score[9])
+    print('Third digit accuracy:', score[10])
+    print('Forth digit accuracy:', score[11])
+    print('Fifth digit accuracy:', score[12])
+
+def predict_sequence(model, pred_input):
+    predictions = model.predict(pred_input)
+
+    pred_legth = np.argmax(predictions[0][0])
+    pred_sequence = ""
+
+    for i in xrange(1, (pred_legth+1)):
+        pred_sequence += str(np.argmax(predictions[i][0]))
+
+    return pred_sequence
+
+
+
+new_model = create_vanilla_CNN(cls_weights)
+trained_model = train_model(model=new_model, train_inputs=X_train,
+                            train_targets=train_targets, test_inputs=X_test,test_targets=test_targets, use_trained=True)
+
+evaluate_model(trained_model,X_test,test_targets)
+
+### Sampling predictions
+fig = plt.figure()
+
+for i in range(1,10):
+    random_index = np.random.choice(10000)
+    prediction = predict_sequence(trained_model, pred_input=X_test[np.newaxis, random_index])
+
+    # Create subplot
+    a = fig.add_subplot(3, 3, i)
+    imgplot = plt.imshow(X_test_orig[random_index])
+    a.set_title('Predicted:'+prediction)
+plt.show()
