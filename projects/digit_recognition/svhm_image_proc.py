9c1c0f0cf7c2ce506b59597495ae5ad838bee9c9
Vadzim Dabravolski <vadzim_dabravolski@epam.com>
2017-02-18 23:58:21 -0500

    complete class for preparing test and training datasets.

--- projects/digit_recognition/svhm_image_proc.py
+++ projects/digit_recognition/svhm_image_proc.py
@@ -5,8 +5,13 @@ from scipy import misc
 
 
 class ImageProcessor(object):
-    def __init__(self, data_path="data/SVHM/train/", extension=1.3, cropped_size=64):
-        self.data_raw = pickle.load(open((data_path + "data.p"), "rb"))
+    def __init__(self, data_path="data/SVHM/train", extension=0.3, cropped_size=64):
+        """
+        :param data_path: location of the data stored. Can be absolute or relative.
+        :param extension: percentage of image extension prior to cropping.
+        :param cropped_size: size of the image in pixel (both vertical and horizontal) after cropping.
+        """
+        self.data_raw = pickle.load(open((data_path + "/data_upd.p"), "rb"))
         self.data_path = data_path
 
         # containers for cropped images
@@ -16,9 +21,9 @@ class ImageProcessor(object):
 
     def _findBBOX(self, data_record):
         """
-        :param
-            data_record - choose between 'train' and 'test'
-        :return: updated_BBOX - coordinates of new BBOX.
+        Method find coordinate of the smallest bounding box which can fit all the individual digits.
+        :param data_record: data record
+        :return: cordinates of the bounding box.
         """
 
         min_top = np.inf
@@ -27,8 +32,8 @@ class ImageProcessor(object):
         max_bottom = -np.inf
 
         for el in data_record['boxes']:
-            if el["height"] < min_top:
-                min_top = el["height"]
+            if el["top"] < min_top:
+                min_top = el["top"]
 
             if el["left"] < min_left:
                 min_left = el["left"]
@@ -38,35 +43,29 @@ class ImageProcessor(object):
 
             if (el["height"] + el['top']) > max_bottom:
                 max_bottom = (el["height"] + el['top'])
+            print("ind digit coord:"+str([min_top, min_left, max_bottom, max_right]))
 
-        crop_coord = [min_top, min_left, max_bottom, max_right]
+        BBOX_coordinates = [min_top, min_left, max_bottom, max_right]
+        print("overal digits coordinates:"+str(BBOX_coordinates))
+        return BBOX_coordinates
 
-        return crop_coord
-
-    def _getUpdatedRecord(self, crop_coord, data_record):
+    def _getUpdatedRecord(self, digits_coord, data_record):
         """
-        Method to return update relative coordinates of the individual digits' bboxes.
-
-        :param crop_coordinate:
+        method returns an updated record for for specific image
+        :param digits_coord:
         :param data_record:
-        :return:
+        :return: relative coordinates of each digit, 1-hot encoded length and digits
         """
-
-
-        rel_top = 0.
-        rel_left = 0.
-        rel_bottom = 0.
-        rel_right = 0.
 
         # transformed features
         coord_relative = []
-        sequence = ''
+        sequence = []
         length = 0
 
-        top = crop_coord[0].astype('float32')
-        bottom = crop_coord[2].astype('float32')
-        left = crop_coord[1].astype('float32')
-        right = crop_coord[3].astype('float32')
+        top = float(digits_coord[0])
+        bottom = float(digits_coord[2])
+        left = float(digits_coord[1])
+        right = float(digits_coord[3])
 
 
         for el in data_record['boxes']:
@@ -75,10 +74,11 @@ class ImageProcessor(object):
             rel_left = (el['left']-left) / (right-left)
             rel_right = (el['left']+el['width']-left) / (right-left)
 
+            print("relative coord:"+str([rel_top, rel_left, rel_bottom, rel_right]))
             coord_relative.append([rel_top, rel_left, rel_bottom, rel_right])
             tmp = np.zeros(11) # 10 digits + empty space (encoded as 10)
             tmp[int(el['label'])] = 1
-            sequence += tmp
+            sequence.append(tmp)
             length += 1
 
         tmp = np.zeros(6)
@@ -87,8 +87,6 @@ class ImageProcessor(object):
         #TODO: handle for empty digits. It's recommended to place their coordinates outside of the bbox
 
         return coord_relative, sequence, length
-
-
 
     def _cropResizeImage(self, image_name, crop_coord):
         """
@@ -99,30 +97,43 @@ class ImageProcessor(object):
         :return: cropped_image
         """
 
-        image = misc.imread(self.data_path+image_name)
+        image = misc.imread(self.data_path+"/"+image_name)
+        image_height = np.shape(image)[0]
+        image_width = np.shape(image)[1]
 
-        # extension of coord
-        top = crop_coord[0].astype('float32') / (self.image_extension / 2)
-        bottom = crop_coord[2].astype('float32') * (self.image_extension / 2)
-        left = crop_coord[1].astype('float32') / (self.image_extension / 2)
-        right = crop_coord[3].astype('float32') * (self.image_extension / 2)
+        # extension of coord TODO: what happens if we get negative coordinate?
+        width_ext = self.image_extension * (crop_coord[3] - crop_coord[1]) / 2
+        height_ext = self.image_extension * (crop_coord[2] - crop_coord[0]) / 2
+        print("height ext:" + str(height_ext))
+        print("width ext:"+str(width_ext))
+        top = max(0, crop_coord[0] - height_ext)
+        bottom = min(image_height, crop_coord[2] + height_ext)
+        left = max(0, crop_coord[1] - width_ext)
+        right = min(image_width, crop_coord[3] + width_ext)
+
+        top = int(top)
+        bottom = int(bottom)
+        left = int(left)
+        right = int(right)
+
+        crop_coord_upd = [top, left, bottom, right] # updating crop coordinates to account for extension.
+        print("crop coordinate after extension:"+str(crop_coord_upd))
 
         # cropping
         image_cropped = image[top:bottom, left:right, :]
 
         # resizing
-        #image_res = misc.imresize(image_cropped, size=[self.cropped_size, self.cropped_size])
-        image_res = misc.imresize(image_cropped, size=(64,64)) #TODO: parametrize it one day
+        image_res = misc.imresize(image_cropped, size=(64,64))
 
         image_res = image_res.astype('float32')
         image_res = image_res / 255  # normalize the image
 
-        return image_res
+        return image_res, crop_coord_upd
 
-    def saveProcessed(self, name_prefix="processed_", rewrite=True):
+    def saveProcessed(self, name_prefix="processed_", rewrite=True, save_image=True):
         """
         This method creates pickle files for processed test and train sets of SVHM images.
-        :return:
+        :return: reference and filenames of the pickle
         """
         processed_filename = self.data_path + name_prefix + "data.p"
         if rewrite:
@@ -131,18 +142,32 @@ class ImageProcessor(object):
 
         for i in xrange(len(self.data_raw)):
             image_name = self.data_raw[i]['filename']
-            crop_coord = self._findBBOX(self.data_raw[i])
-            image_resized = self._cropResizeImage(image_name, crop_coord)
-            coord_updated, sequence_label, length = self._getUpdatedRecord(crop_coord, self.data_raw[i])
+            print(str(self.data_raw[i]['boxes']))
+            digits_coord = self._findBBOX(self.data_raw[i])
+            image_resized, digits_coord = self._cropResizeImage(image_name, digits_coord)
+            coord_updated, sequence_label, length = self._getUpdatedRecord(digits_coord, self.data_raw[i])
 
-            self.dataset_cropped.append([image_resized, coord_updated, length, sequence_label])
+            self.dataset_cropped.append([image_name, image_resized, coord_updated, length,sequence_label])
+            #record = {'image_name': image_name, 'image': image_resized, 'coordinates': coord_updated, 'length': length, 'label': sequence_label}
+            #self.dataset_cropped.append(record)
 
 
-        #TODO: save to pickle and test.
+            if save_image:
+                misc.imsave(self.data_path+"_cropped/"+image_name, image_resized)
+
+        datafile = self.data_path+"_cropped/"+"dataset.p"
+        pickle.dump(self.dataset_cropped, open(datafile, "wb"))
+        return datafile
 
 
-proc = ImageProcessor(data_path="data/SVHM/test/")
-proc.saveProcessed()
+
+
+proc_test = ImageProcessor(data_path="data/SVHM/test")
+cropped_test = proc_test.saveProcessed()
+
+proc_train = ImageProcessor(data_path="data/SVHM/train")
+cropped_train = proc_train.saveProcessed()
+
 
 
 
