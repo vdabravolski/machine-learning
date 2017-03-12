64a280fd0e6e113fce3aa5eda547197e4511c7f9
vadim <vadim@Vadzims-MacBook-Pro.local>
2017-03-12 15:44:55 -0400

    problem 2. basic debug passed. need more tuning

--- projects/digit_recognition/digit_recognition_models.py
+++ projects/digit_recognition/digit_recognition_models.py
@@ -7,11 +7,8 @@ Features:
 
 from __future__ import print_function
 import numpy as np
-
-np.random.seed(1337)  # for reproducibility
-
 from keras.datasets import mnist
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Input
 from keras.layers import Convolution2D, MaxPooling2D
@@ -20,71 +17,147 @@ from keras.utils import np_utils
 from keras import backend as K
 from mnist_data_preps import generate_sequences
 from matplotlib import pyplot as plt
+import pickle
+import tensorflow as tf
+import datetime
+import os
+tf.python.control_flow_ops = tf # to overcome some compatibility issues between keras and tf
 
+
+np.random.seed(1337)  # for reproducibility
 batch_size = 128
 nb_classes = 11  # include 10 digits and "-1" which designates empty space in digit sequence.
 nb_epoch = 12
 sequence_length = 5
 
 # input image dimensions
-img_rows, img_cols = 28, (28 * sequence_length)
+# img_rows, img_cols = 28, (28 * sequence_length) # this is for MNIST
+img_rows, img_cols, img_depth = 64, 64, 3 # this is for SVHM
 # number of convolutional filters to use
 nb_filters = 32
 # size of pooling area for max pooling
 pool_size = (2, 2)
 # convolution kernel size
 kernel_size = (3, 3)
-weights_path = "tmp/vanilla_cnn_weights.hdf5"
+pretrained_weights = "tmp/vanilla_cnn_weights.hdf5"
 
-# the data, shuffled and split between train and test sets
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+def prepare_MNIST_data():
+    # the data, shuffled and split between train and test sets
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-X_train_orig, Y_train, Y_train_length = generate_sequences(train_images, train_labels, out_size=np.shape(train_images)[0],
-                                                      max_length=sequence_length)
-X_test_orig, Y_test, Y_test_length = generate_sequences(test_images, test_labels, out_size=np.shape(test_images)[0],
-                                                   max_length=sequence_length)
+    X_train_orig, Y_train, Y_train_length = generate_sequences(train_images, train_labels, out_size=np.shape(train_images)[0],
+                                                          max_length=sequence_length)
+    X_test_orig, Y_test, Y_test_length = generate_sequences(test_images, test_labels, out_size=np.shape(test_images)[0],
+                                                       max_length=sequence_length)
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train_orig.reshape(X_train_orig.shape[0], 1, img_rows, img_cols)
-    X_test = X_test_orig.reshape(X_test_orig.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train_orig.reshape(X_train_orig.shape[0], img_rows, img_cols, 1)
-    X_test = X_test_orig.reshape(X_test_orig.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    if K.image_dim_ordering() == 'th':
+        X_train = X_train_orig.reshape(X_train_orig.shape[0], 1, img_rows, img_cols)
+        X_test = X_test_orig.reshape(X_test_orig.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        X_train = X_train_orig.reshape(X_train_orig.shape[0], img_rows, img_cols, 1)
+        X_test = X_test_orig.reshape(X_test_orig.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
 
-# Classifiers' inputs and targets
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+    # Classifiers' inputs and targets
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
 
-# 1. Length classifier
-length_cls_train = Y_train_length
-length_cls_test = Y_test_length
+    # 1. Length classifier
+    length_cls_train = Y_train_length
+    length_cls_test = Y_test_length
 
-# 2. First digit classifier
-first_cls_train = Y_train[:, 0, :]
-first_cls_test = Y_test[:, 0, :]
+    # 2. First digit classifier
+    first_cls_train = Y_train[:, 0, :]
+    first_cls_test = Y_test[:, 0, :]
 
-# 3.Second digit classifier
-second_cls_train = Y_train[:, 1, :]
-second_cls_test = Y_test[:, 1, :]
+    # 3.Second digit classifier
+    second_cls_train = Y_train[:, 1, :]
+    second_cls_test = Y_test[:, 1, :]
 
-# 4. Third digit classifier
-third_cls_train = Y_train[:, 2, :]
-third_cls_test = Y_test[:, 2, :]
+    # 4. Third digit classifier
+    third_cls_train = Y_train[:, 2, :]
+    third_cls_test = Y_test[:, 2, :]
 
-# 5. Forth digit classifier
-forth_cls_train = Y_train[:, 3, :]
-forth_cls_test = Y_test[:, 3, :]
+    # 5. Forth digit classifier
+    forth_cls_train = Y_train[:, 3, :]
+    forth_cls_test = Y_test[:, 3, :]
 
-# 6. First digit classifier
-fifth_cls_train = Y_train[:, 4, :]
-fifth_cls_test = Y_test[:, 4, :]
+    # 6. First digit classifier
+    fifth_cls_train = Y_train[:, 4, :]
+    fifth_cls_test = Y_test[:, 4, :]
 
-train_targets = [length_cls_train, first_cls_train, second_cls_train, third_cls_train, forth_cls_train, fifth_cls_train]
-test_targets = [length_cls_test, first_cls_test, second_cls_test, third_cls_test, forth_cls_test, fifth_cls_test]
-cls_weights = [1., 1., 1., 1., 1., 1.]
+    train_targets = [length_cls_train, first_cls_train, second_cls_train, third_cls_train, forth_cls_train, fifth_cls_train]
+    test_targets = [length_cls_test, first_cls_test, second_cls_test, third_cls_test, forth_cls_test, fifth_cls_test]
 
+    return X_train, train_targets, X_test, test_targets, input_shape
+
+def prepare_SVHM_data():
+
+    test_data = "./data/SVHM/test_processed/dataset.p"
+    test_data = pickle.load(open(test_data, 'rb'))
+
+    train_data = "./data/SVHM/train_processed/dataset.p"
+    train_data = pickle.load(open(train_data, 'rb'))
+
+    #Images
+    # X_train = [el[1] for el in test_data]
+    # X_test = [el[1] for el in train_data]
+    X_train = np.zeros(shape=(len(train_data), img_rows, img_cols, img_depth))
+    X_test = np.zeros(shape=(len(test_data), img_rows, img_cols, img_depth))
+
+    # 1. Length
+    # lenght_train = [el[3] for el in test_data]
+    # lenght_test = [el[3] for el in train_data]
+    length_train = np.zeros(shape=(len(train_data), 6))
+    length_test = np.zeros(shape=(len(test_data), 6))
+
+
+    # 2. Coordinates
+    # coord_train = [np.asarray(el[2]) for el in train_data]
+    # coord_test = [np.asarray(el[2]) for el in test_data]
+    coord_train = np.zeros(shape=(len(train_data), 20)) # here 20 = 5 (max number of digits) * 4 (coordinates for each digit)
+    coord_test = np.zeros(shape=(len(test_data), 20))
+
+
+    # 3. Digits
+    first_train = np.zeros(shape=(len(train_data), 11))
+    second_train = np.zeros(shape=(len(train_data), 11))
+    third_train = np.zeros(shape=(len(train_data), 11))
+    forth_train = np.zeros(shape=(len(train_data), 11))
+    fifth_train = np.zeros(shape=(len(train_data), 11))
+
+    first_test = np.zeros(shape=(len(test_data), 11))
+    second_test = np.zeros(shape=(len(test_data), 11))
+    third_test = np.zeros(shape=(len(test_data), 11))
+    forth_test = np.zeros(shape=(len(test_data), 11))
+    fifth_test = np.zeros(shape=(len(test_data), 11))
+
+    for idx, el in enumerate(test_data):
+        X_test[idx, :, :, :] = el[1]
+        length_test[idx, :] = el[3]
+        coord_test[idx, :] = np.asarray(el[2]).flatten()
+        first_test[idx, :] = el[4][0]
+        second_test[idx, :] = el[4][1]
+        third_test[idx, :] = el[4][2]
+        forth_test[idx, :] = el[4][3]
+        fifth_test[idx, :] = el[4][4]
+
+    for idx, el in enumerate(train_data):
+        X_train[idx, :, :, :] = el[1]
+        length_train[idx, :] = el[3]
+        coord_train[idx, :] = np.asarray(el[2]).flatten()
+        first_train[idx, :] = el[4][0]
+        second_train[idx, :] = el[4][1]
+        third_train[idx, :] = el[4][2]
+        forth_train[idx, :] = el[4][3]
+        fifth_train[idx, :] = el[4][4]
+
+    test_targets = [length_test, first_test, second_test, third_test, forth_test, fifth_test, coord_test]
+    train_targets = [length_train, first_train, second_train, third_train, forth_train, fifth_train, coord_train]
+    input_shape = (img_rows, img_cols, img_depth)
+
+    return X_train, train_targets, X_test, test_targets, input_shape
 
 def synth_MNIST_model(cls_weights):
     # Shared feature layers which will be used for multiple classifiers.
@@ -129,33 +202,148 @@ def synth_MNIST_model(cls_weights):
     # model compilation and training
     model = Model(input=[main_input], output=[length_cls, first_cls, second_cls, third_cls, forth_cls, fifth_cls])
 
-    model.compile(loss='categorical_crossentropy',
+    model.compile(loss={'categorical_crossentropy':[length_cls, first_cls, second_cls, third_cls, forth_cls, fifth_cls]},
                   optimizer='adadelta',
                   metrics=['accuracy'], loss_weights=cls_weights)
 
     return model
 
+def SVHM_model(cls_weight):
+    # Shared feature layers which will be used for multiple classifiers.
+    main_input = Input(shape=input_shape, dtype='float32', name='main_input')
+    shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                           border_mode='valid', input_shape=input_shape)(main_input)
+    shared = Activation('relu')(shared)
+    shared = Convolution2D(nb_filters, kernel_size[0], kernel_size[1])(shared)
+    shared = Activation('relu')(shared)
+    shared = MaxPooling2D(pool_size=pool_size)(shared)
+    shared = Dropout(0.25)(shared)
+    shared = Flatten()(shared)
+    shared = Dense(128)(shared)
+    shared = Activation('relu')(shared)
+    shared = Dropout(0.5)(shared)
+
+
+    # 6 different classifiers.
+    # 1. Length classifier
+    length_cls = Dense((sequence_length + 1))(shared)  # to account for sequence length + 1
+    length_cls = Activation('softmax', name="length_cls")(length_cls)
+
+    # 2. First digit classifier
+    first_cls = Dense(nb_classes)(shared)
+    first_cls = Activation('softmax', name="first_cls")(first_cls)
+
+    # 3. Second digit classifier
+    second_cls = Dense(nb_classes)(shared)
+    second_cls = Activation('softmax', name="second_cls")(second_cls)
+
+    # 4. Third digit classifier
+    third_cls = Dense(nb_classes)(shared)
+    third_cls = Activation('softmax', name="third_cls")(third_cls)
+
+    # 5. Forth digit classifier
+    forth_cls = Dense(nb_classes)(shared)
+    forth_cls = Activation('softmax', name="forth_cls")(forth_cls)
+
+    # 6. Fifth digit classifier
+    fifth_cls = Dense(nb_classes)(shared)
+    fifth_cls = Activation('softmax', name="fifth_cls")(fifth_cls)
+
+    #7. Digit boxes coordinates regresssion
+    coord_regr = Dense(20, name="coord_regr")(shared)
+
+    # model compilation and training
+    model = Model(input=[main_input], output=[length_cls, first_cls, second_cls, third_cls, forth_cls, fifth_cls, coord_regr])
+
+    model.compile(loss={'length_cls': 'categorical_crossentropy', 'first_cls': 'categorical_crossentropy',
+                        'second_cls': 'categorical_crossentropy', 'third_cls': 'categorical_crossentropy',
+                        'forth_cls': 'categorical_crossentropy', 'fifth_cls': 'categorical_crossentropy',
+                        'coord_regr': 'mean_squared_error'},
+                  optimizer='adadelta',
+                  metrics={'length_cls': 'accuracy', 'first_cls': 'accuracy',
+                           'second_cls': 'accuracy', 'third_cls': 'accuracy',
+                           'forth_cls': 'accuracy', 'fifth_cls': 'accuracy',
+                           'coord_regr': _iou_metric}, loss_weights=cls_weights)
+
+    return model
+
+def _iou_metric(y_true, y_pred, epsilon=1e-5, sequence_length=5):
+    """ Inspired by: http://ronny.rest/tutorials/module/localization_001/intersect_of_union/
+
+        Given two arrays `y_true` and `y_pred` where each row contains a bounding
+        boxes for sequence of digits. By default, sequence length is 5. Each digit is represented by 4 numbers:
+            [y1, x1, y2, x2]
+        where:
+            x1,y1 represent the upper left corner
+            x2,y2 represent the lower right corner
+        It returns the Intersect of Union scores for each sequence.
+
+    Args:
+        y_true:          (numpy array) sequence_length * each row containing [y1, x1, y2, x2] coordinates
+        y_pred:          (numpy array) sequence_length * each row containing [y1, x1, y2, x2] coordinates
+        epsilon:    (float) Small value to prevent division by zero
+        sequence_length: (int) number of digits in the sequence
+
+    Returns:
+        (float) Sum of IoU for all digits in sequence
+    """
+
+    # Reshape the sequence coordinates which comes flatten from the regressor.
+    y_true = K.reshape(y_true, [-1, 5, 4])
+    y_pred = K.reshape(y_pred, [-1, 5, 4])
+
+    K.print_tensor(y_true, "OLOLO")
+
+    # COORDINATES OF THE INTERSECTION BOXES
+    y1 = K.maximum(y_true[:, :, 0], y_pred[:, :, 0])
+    x1 = K.maximum(y_true[:, :, 1], y_pred[:, :, 1])
+    y2 = K.minimum(y_true[:, :, 2], y_pred[:, :, 2])
+    x2 = K.minimum(y_true[:, :, 3], y_pred[:, :, 3])
+
+    # AREAS OF OVERLAP - Area where the boxes intersect
+    width = (x2 - x1)
+    height = (y2 - y1)
+
+    # handle case where there is NO overlap
+    width = K.clip(width, 0, None)
+    height = K.clip(height, 0, None)
+
+    #area_overlap = width * height
+    #area_overlap = K.prod(width * height)
+    area_overlap = K.tf.multiply(width, height)
+
+    # COMBINED AREAS
+    area_a = (y_true[:, :, 2] - y_true[:, :, 0]) * (y_true[:, :, 3] - y_true[:, :, 1])
+    area_b = (y_pred[:, :, 2] - y_pred[:, :, 0]) * (y_pred[:, :, 3] - y_pred[:, :, 1])
+    area_combined = area_a + area_b - area_overlap
+
+    # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+    iou = area_overlap / (area_combined + epsilon)
+    iou = K.mean(iou) # reduce mean across all axis
+
+    return iou
 
 def train_model(model, train_inputs, train_targets, test_inputs, test_targets, use_trained=False):
     if use_trained:
         model = model
-        model.load_weights(weights_path)
+        model.load_weights(pretrained_weights)
     else:
-        checkpointer = ModelCheckpoint(filepath=weights_path, verbose=1, save_best_only=True)  # TODO: add relative path
+        #create appropriate directory to store  weights and logs.
+        directory = "output/" + str(datetime.datetime.now())
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        checkpointer = ModelCheckpoint(filepath=directory+"/weights.hdf5", verbose=1, save_best_only=True)
+        tensorboard = TensorBoard(log_dir=directory, histogram_freq=0, write_graph=True, write_images=False)
         model.fit([train_inputs], train_targets, batch_size=batch_size, nb_epoch=nb_epoch,
-                  verbose=1, validation_data=([test_inputs], test_targets), callbacks=[checkpointer])
+                  verbose=1, validation_data=([test_inputs], test_targets), callbacks=[checkpointer, tensorboard])
 
     return model
 
-
 def evaluate_model(model, eval_inputs, eval_targets, verbose=1):
     score = model.evaluate(eval_inputs, eval_targets, verbose=verbose)
-    print('Length accuracy:', score[7])
-    print('First digit accuracy:', score[8])
-    print('Second digit accuracy:', score[9])
-    print('Third digit accuracy:', score[10])
-    print('Forth digit accuracy:', score[11])
-    print('Fifth digit accuracy:', score[12])
+    for idx in xrange(len(score)):
+        print(model.metrics_names[idx]+": "+str(score[idx]))
 
 def predict_sequence(model, pred_input):
     predictions = model.predict(pred_input)
@@ -170,21 +358,32 @@ def predict_sequence(model, pred_input):
 
 
 
-new_model = synth_MNIST_model(cls_weights)
+# Training SVHM model
+X_train, train_targets, X_test, test_targets, input_shape = prepare_SVHM_data()
+cls_weights = [1., 1., 1., 1., 1., 1., 1.]
+new_model = SVHM_model(cls_weights)
 trained_model = train_model(model=new_model, train_inputs=X_train,
-                            train_targets=train_targets, test_inputs=X_test,test_targets=test_targets, use_trained=True)
+                            train_targets=train_targets, test_inputs=X_test,
+                            test_targets=test_targets, use_trained=False)
+evaluate_model(trained_model, X_test, test_targets)
 
-evaluate_model(trained_model,X_test,test_targets)
 
-### Sampling predictions
-fig = plt.figure()
-
-for i in range(1,10):
-    random_index = np.random.choice(10000)
-    prediction = predict_sequence(trained_model, pred_input=X_test[np.newaxis, random_index])
-
-    # Create subplot
-    a = fig.add_subplot(3, 3, i)
-    imgplot = plt.imshow(X_test_orig[random_index])
-    a.set_title('Predicted:'+prediction)
-plt.show()
+# Training MNIST model
+# new_model = synth_MNIST_model(cls_weights)
+# trained_model = train_model(model=new_model, train_inputs=X_train,
+#                             train_targets=train_targets, test_inputs=X_test,test_targets=test_targets, use_trained=True)
+#
+# evaluate_model(trained_model,X_test,test_targets)
+#
+# ### Sampling predictions
+# fig = plt.figure()
+#
+# for i in range(1,10):
+#     random_index = np.random.choice(10000)
+#     prediction = predict_sequence(trained_model, pred_input=X_test[np.newaxis, random_index])
+#
+#     # Create subplot
+#     a = fig.add_subplot(3, 3, i)
+#     imgplot = plt.imshow(X_test_orig[random_index]) #TODO handle this case
+#     a.set_title('Predicted:'+prediction)
+# plt.show()
--- projects/digit_recognition/svhm_image_proc.py
+++ projects/digit_recognition/svhm_image_proc.py
@@ -5,13 +5,13 @@ from scipy import misc
 
 
 class ImageProcessor(object):
-    def __init__(self, data_path="data/SVHM/train", extension=0.3, cropped_size=64):
+    def __init__(self, data_path="data/SVHM/train", data_file="dataset.p", extension=0.3, cropped_size=64):
         """
         :param data_path: location of the data stored. Can be absolute or relative.
         :param extension: percentage of image extension prior to cropping.
         :param cropped_size: size of the image in pixel (both vertical and horizontal) after cropping.
         """
-        self.data_raw = pickle.load(open((data_path + "/data_upd.p"), "rb"))
+        self.data_raw = pickle.load(open((data_path + "/" + data_file), "rb"))
         self.data_path = data_path
 
         # containers for cropped images
@@ -38,15 +38,15 @@ class ImageProcessor(object):
             if el["left"] < min_left:
                 min_left = el["left"]
 
-            if (el['left']+el["width"]) > max_right:
-                max_right = el['left']+el["width"]
+            if (el['left'] + el["width"]) > max_right:
+                max_right = el['left'] + el["width"]
 
             if (el["height"] + el['top']) > max_bottom:
                 max_bottom = (el["height"] + el['top'])
-            print("ind digit coord:"+str([min_top, min_left, max_bottom, max_right]))
+            print("ind digit coord:" + str([min_top, min_left, max_bottom, max_right]))
 
         BBOX_coordinates = [min_top, min_left, max_bottom, max_right]
-        print("overal digits coordinates:"+str(BBOX_coordinates))
+        print("overal digits coordinates:" + str(BBOX_coordinates))
         return BBOX_coordinates
 
     def _getUpdatedRecord(self, digits_coord, data_record):
@@ -62,29 +62,35 @@ class ImageProcessor(object):
         sequence = []
         length = 0
 
+        # blank coordinates for non-existent digits. according to: https://discussions.udacity.com/t/tips-for-svhn-project-with-bounding-boxes/219969
+        coord_relative = np.zeros(shape=(5, 4), dtype='float32')
+
+        # initialize empty sequence array
+        sequence = np.zeros(shape=(5, 11), dtype='float32')
+        sequence[:, 10] = 1  # by default assign placeholder digit.
+
         top = float(digits_coord[0])
         bottom = float(digits_coord[2])
         left = float(digits_coord[1])
         right = float(digits_coord[3])
 
+        for idx, el in enumerate(data_record['boxes']):
+            rel_top = (el['top'] - top) / (bottom - top)
+            rel_bottom = (el['top'] + el['height'] - top) / (bottom - top)
+            rel_left = (el['left'] - left) / (right - left)
+            rel_right = (el['left'] + el['width'] - left) / (right - left)
 
-        for el in data_record['boxes']:
-            rel_top = (el['top']-top) / (bottom-top)
-            rel_bottom = (el['top']+el['height']-top) / (bottom-top)
-            rel_left = (el['left']-left) / (right-left)
-            rel_right = (el['left']+el['width']-left) / (right-left)
-
-            print("relative coord:"+str([rel_top, rel_left, rel_bottom, rel_right]))
-            coord_relative.append([rel_top, rel_left, rel_bottom, rel_right])
-            tmp = np.zeros(11) # 10 digits + empty space (encoded as 10)
+            print("relative coord:" + str([rel_top, rel_left, rel_bottom, rel_right]))
+            # coord_relative.append([rel_top, rel_left, rel_bottom, rel_right])
+            coord_relative[idx, :] = np.asarray([rel_top, rel_left, rel_bottom, rel_right])
+            tmp = np.zeros(11)  # 10 digits + empty space (encoded as 10)
             tmp[int(el['label'])] = 1
-            sequence.append(tmp)
+            sequence[idx, :] = tmp
             length += 1
 
         tmp = np.zeros(6)
         tmp[length] = 1
         length = tmp
-        #TODO: handle for empty digits. It's recommended to place their coordinates outside of the bbox
 
         return coord_relative, sequence, length
 
@@ -97,15 +103,15 @@ class ImageProcessor(object):
         :return: cropped_image
         """
 
-        image = misc.imread(self.data_path+"/"+image_name)
+        image = misc.imread(self.data_path + "/" + image_name)
         image_height = np.shape(image)[0]
         image_width = np.shape(image)[1]
 
         # extension of coord TODO: what happens if we get negative coordinate?
         width_ext = self.image_extension * (crop_coord[3] - crop_coord[1]) / 2
         height_ext = self.image_extension * (crop_coord[2] - crop_coord[0]) / 2
-        print("height ext:" + str(height_ext))
-        print("width ext:"+str(width_ext))
+        #print("height ext:" + str(height_ext))
+        #print("width ext:" + str(width_ext))
         top = max(0, crop_coord[0] - height_ext)
         bottom = min(image_height, crop_coord[2] + height_ext)
         left = max(0, crop_coord[1] - width_ext)
@@ -116,64 +122,62 @@ class ImageProcessor(object):
         left = int(left)
         right = int(right)
 
-        crop_coord_upd = [top, left, bottom, right] # updating crop coordinates to account for extension.
-        print("crop coordinate after extension:"+str(crop_coord_upd))
+        crop_coord_upd = [top, left, bottom, right]  # updating crop coordinates to account for extension.
+        #print("crop coordinate after extension:" + str(crop_coord_upd))
 
         # cropping
         image_cropped = image[top:bottom, left:right, :]
 
         # resizing
-        image_res = misc.imresize(image_cropped, size=(64,64))
+        image_res = misc.imresize(image_cropped, size=(64, 64))
 
         image_res = image_res.astype('float32')
         image_res = image_res / 255  # normalize the image
 
         return image_res, crop_coord_upd
 
-    def saveProcessed(self, name_prefix="processed_", rewrite=True, save_image=True):
+    def saveProcessed(self, folder_postfix="_processed", rewrite=True, save_image=True, debugSample=False):
         """
         This method creates pickle files for processed test and train sets of SVHM images.
         :return: reference and filenames of the pickle
         """
-        processed_filename = self.data_path + name_prefix + "data.p"
+        new_folder = self.data_path + folder_postfix + "/"
+        processed_filename = new_folder + "data.p"
+
         if rewrite:
             if os.path.isfile(processed_filename):
                 os.remove(processed_filename)
 
-        for i in xrange(len(self.data_raw)):
+        if debugSample:
+            iterations = 400
+        else:
+            iterations = len(self.data_raw)
+
+        for i in xrange(iterations):
             image_name = self.data_raw[i]['filename']
             print(str(self.data_raw[i]['boxes']))
             digits_coord = self._findBBOX(self.data_raw[i])
             image_resized, digits_coord = self._cropResizeImage(image_name, digits_coord)
             coord_updated, sequence_label, length = self._getUpdatedRecord(digits_coord, self.data_raw[i])
 
-            self.dataset_cropped.append([image_name, image_resized, coord_updated, length,sequence_label])
-            #record = {'image_name': image_name, 'image': image_resized, 'coordinates': coord_updated, 'length': length, 'label': sequence_label}
-            #self.dataset_cropped.append(record)
+            self.dataset_cropped.append([image_name, image_resized, coord_updated, length, sequence_label])
+            # record = {'image_name': image_name, 'image': image_resized, 'coordinates': coord_updated, 'length': length, 'label': sequence_label}
+            # self.dataset_cropped.append(record)
 
 
             if save_image:
-                misc.imsave(self.data_path+"_cropped/"+image_name, image_resized)
+                misc.imsave(new_folder + image_name, image_resized)
 
-        datafile = self.data_path+"_cropped/"+"dataset.p"
+        datafile = new_folder + "dataset.p"
         pickle.dump(self.dataset_cropped, open(datafile, "wb"))
         return datafile
 
 
+# proc_debug = ImageProcessor(data_path="data/SVHM/test", data_file="test_data.p")
+# proc_debug.saveProcessed(folder_postfix="_debug", debugSample=True)
 
-
-proc_test = ImageProcessor(data_path="data/SVHM/test")
+proc_test = ImageProcessor(data_path="data/SVHM/test", data_file="dataset.p")
 cropped_test = proc_test.saveProcessed()
 
-proc_train = ImageProcessor(data_path="data/SVHM/train")
-cropped_train = proc_train.saveProcessed()
-
-
-
-
-
-
-
-
-
-
+proc_train = ImageProcessor(data_path="data/SVHM/train", data_file="dataset.p")
+cropped_train = proc_train.saveProcessed(save_image=False)
