34399d33a3f331bd89a30a2f0c8a83a6568202ae
vadim <vadim@Vadzims-MacBook-Pro.local>
2017-09-06 00:21:13 -0400

    model is working on TS data. Likely overfitting though.

--- projects/capstone/model.py
+++ projects/capstone/model.py
@@ -15,64 +15,99 @@ from keras.layers import Dense, Merge
 from keras.layers.normalization import BatchNormalization
 import math
 import pandas as pd
+import random
+import json
+import os
+import uuid
 
 TIMESERIES_FOLDER = "data/timeseries/"
 
 
-def get_ts_ticker_data(ticker, input_dim, train_test_ratio=0.7, sliding_window=True):
-    """method to do necessary data massage"""
+def get_ts_ticker_data(ticker, output_shape, train_test_ratio=0.7, sliding_window=True, classification=True):
+    """method to do necessary data massage
+    sliding window - if True, then input_dim[0] sliding windows is created;
+    output_shape:
+        defined the output shape of X samples;
+        Y outputs is (output_shape[0], 1), where 1 - is exactly one value in the end of each time series.
+    classification - defined if Y should contain boolean value (1 if stock grow, 0 if fell.
+                    if classification = False, then stock price (float) is returned.
+    """
 
     file = TIMESERIES_FOLDER + "{0}_df.p".format(ticker)
     with open(file, "rb") as file:
         ticker_df = pickle.load(file)
 
-    # explicitly define input dimensions
-    batch_size = input_dim[0]
-    timesteps = input_dim[1]
-    features = input_dim[2]
-
-    augmented_df = pd.DataFrame(columns=ticker_df.columns)
+    data_df = pd.DataFrame(columns=ticker_df.columns)
+    target_df = pd.DataFrame(columns=['diff_bool', 'value'])
 
     if sliding_window:  # implementation of sliding window. Each window is moving by 1 element.
-        for idx in range(ticker_df.shape[0] - timesteps):
-            augmented_df = augmented_df.append(ticker_df[idx: idx + timesteps])
-            # for idx_ts in range(timesteps):
-            #     combined_idx = idx_df+idx_ts
 
-    augmented_df.shape
+        if len(output_shape) != 3:
+            raise ValueError("If you choose sliding window parameter"
+                             "please specify 3 output dimensions for data."
+                             "Now you passed {0}".format(len(output_shape)))
 
-    # Assume that we use "close" price as an indicator whether the stock is up and down.
-    Y = augmented_df.close[:-1].reset_index(drop=True) < augmented_df.close[1:].reset_index(drop=True)
-    Y = Y.astype(int)
-    print(augmented_df.columns)
-    augmented_df = augmented_df.reset_index(drop=True)
-    X = augmented_df.loc[:(augmented_df.shape[0] - 2),
-        ['open', 'high', 'low', 'close', 'volume']]  # TODO: reduced number of features is used.
+        # Explicitly define output dimensions
+        batch_size = output_shape[0]
+        timesteps = output_shape[1]
+        features = output_shape[2]
 
-    # TODO: right now sector_id is not one-hot encoded.
-    train_index = int(X.shape[0] * train_test_ratio)
+        for idx in range(ticker_df.shape[0] - timesteps-1):
 
-    # TODO: P0 with sliding window approach, labels are off on the boundaries.
-    Y_train = Y.values[:train_index]
-    # Y_train = to_categorical(Y_train, num_classes=2)
+            data_sample = ticker_df[idx: idx + timesteps]
+            diff = int(ticker_df.close[idx + timesteps + 1] > ticker_df.close[idx + timesteps])
+            target_sample = pd.DataFrame([[diff, ticker_df.close[idx + timesteps+1]]],
+                                         columns=['diff_bool', 'value'])
 
-    Y_test = Y.values[(train_index + 1):]
-    # Y_test = to_categorical(Y_test, num_classes=2)
+            data_df = data_df.append(data_sample)
+            target_df = target_df.append(target_sample, ignore_index=True)
 
-    X_train = X.values[:train_index, :]
-    X_test = X.values[(train_index + 1):, :]
+    else:
 
-    # TODO: when resizing, we are trimming a lot of data to fit into the batch*timestep dimensions.
+        raise NotImplemented("Shaping time-series data without sliding windows are to be implemented if needed.")
+
+        if len(output_shape) != 2:
+            raise ValueError("Please specify output with shape (batch_size, feature_size)"
+                             "Now you passed {0}".format(len(output_shape)))
+
+
+        # Explicitly define output dimensions
+        batch_size = output_shape[0]
+        features = output_shape[1]
+
+
+    if classification:
+        Y = target_df.diff_bool
+    else:
+        Y = target_df.value
+
+
+    data_df = data_df.reset_index(drop=True)
+    X = data_df.loc[:,['open', 'high', 'low', 'close', 'volume']]  # TODO: reduced number of features are used.
+
+    X_train_index = int(X.shape[0] * train_test_ratio)
+    Y_train_index = int(Y.shape[0] * train_test_ratio)
+
+    Y_train = Y.values[:Y_train_index]
+    Y_test = Y.values[(Y_train_index + 1):]
+
+    X_train = X.values[:X_train_index, :]
+    X_test = X.values[(X_train_index + 1):, :]
+
+
+
+    # TODO: when resizing, we are trimming a lot of data to fit into the batch*timestep size.
     X_train = _resize_data_for_batches(X_train, batch_size * timesteps)
     X_test = _resize_data_for_batches(X_test, batch_size * timesteps)
-    Y_train = _resize_data_for_batches(Y_train, batch_size * timesteps)
-    Y_test = _resize_data_for_batches(Y_test, batch_size * timesteps)
-
-    # resize to batches
     X_train = np.reshape(X_train, (-1, timesteps, features))
     X_test = np.reshape(X_test, (-1, timesteps, features))
-    Y_train = np.reshape(Y_train, (-1, timesteps))
-    Y_test = np.reshape(Y_test, (-1, timesteps))
+
+    Y_train = Y_train[:X_train.shape[0]]
+    Y_test = Y_test[:X_test.shape[0]]
+
+
+    # resize to batches
+
 
     print("Input data shape: \n X train: {0}, Y train: {1} \n X test: {2}, Y test: {3}".
           format(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape))
@@ -85,40 +120,42 @@ def get_complex_model():
     # A dictionary with specific config. Will be used to track experiments settings.
     model_config = {'name': 'complex_model'}
     model_config['optimizer'] = 'adam'
-    model_config['loss'] = 'binary_crossentropy'
+    #model_config['loss'] = 'binary_crossentropy'
+    model_config['loss'] = 'mean_squared_error'
 
     # TODO: in future add here nlp and fundamentals branches
     model, ts_config = ts_model_branch()
     model_config['ts_config'] = ts_config
-    model.compile(loss=model_config['loss'], optimizer=model_config['optimizer'], metrics=['accuracy'])
+    model.compile(loss=model_config['loss'], optimizer=model_config['optimizer'])
 
     return model, model_config
 
 def ts_model_branch():
     c = {'name': 'ts_branch'}
-    c['lstm2_shape'] = [100]  # TODO: #2 element is inline with with batch size.
-    c['activation'] = 'softmax'
-    c['lstm_units'] = 5
-    c['batch_size'] = 128
-    c['timesteps'] = 50
-    c['return_sequence'] = False
+    c['batch_size'] = batch_size
+    c['timesteps'] = timesteps
+    c['features'] = 5  # TODO: this is a hardcoded value. Need to come up with not hardcoded alternative.
+    c['lstm_shapes'] = [100, 50, 20]  # TODO: #2 element is inline with with batch size.
+    c['activation'] = 'linear'
+    c['go_backwards'] = True
+    c['dense_shapes'] = [20, 1]
+    c['return_sequence'] = True
     c['stateful'] = True
-    c['features'] = 5  # TODO: this is a hardcoded value. Need to come up with not hardcoded alternative. Thi defines input dimension.
     c['output'] = 1
 
-    # # TODO: likely, this will work only for timesteps=1. Need to rethink assignment of predicted values given the timestep.
     ## Explanations of return_sequence and TimeDistributed: https://stackoverflow.com/questions/42755820/how-to-use-return-sequences-option-and-timedistributed-layer-in-keras
     ## EXplanations of stateful parameter: http://philipperemy.github.io/keras-stateful-lstm/
+    ## Good article on different modes of RNN (one-to-many, many-to-many etc.)
+
     branch = Sequential()
-    branch.add(LSTM(c['lstm_units'], batch_input_shape=(c['batch_size'], c['timesteps'], c['features']),
-                    return_sequences=True, stateful=c['stateful'], name="lstm_1"))
-    branch.add(Dropout(0.2, name="dropout_1"))
-    branch.add(LSTM(c['lstm_units'],
-                    return_sequences=False, name="lstm_2", stateful=c['stateful']))
-    branch.add(Dropout(0.2, name="dropout_2"))
-    branch.add(Dense(units=c['output'], activation=c['activation'],
-               name="dense_1"))
-    # branch.add(Activation(c['activation'], name="activation_test_1"))
+    branch.add(LSTM(c['lstm_shapes'][0], batch_input_shape=(c['batch_size'], c['timesteps'], c['features']),
+                    return_sequences=c['return_sequence'], stateful=c['stateful'], name="lstm_1", go_backwards=c['go_backwards']))
+    branch.add(LSTM(c['lstm_shapes'][0],
+                    return_sequences=c['return_sequence'], name="lstm_2", stateful=c['stateful']))
+    branch.add(LSTM(c['lstm_shapes'][0],
+                    return_sequences=False, name="lstm_3", stateful=c['stateful']))
+    branch.add(Dense(units=c['dense_shapes'][0], activation=c['activation'], name="dense_1"))
+    branch.add(Dense(units=c['dense_shapes'][1], activation=c['activation'], name="dense_2"))
 
     return branch, c
 
@@ -143,20 +180,21 @@ def _resize_data_for_batches(data, batch_size):
     return data[:new_sample_size]
 
 
-def training_model(ticker):
+
+def training_model(ticker, result_folder):
     """method to train the model"""
 
     # Initialize complex model.
     model, model_config = get_complex_model()
-    model_config['epochs'] = 10
+    model_config['epochs'] = 1
+    model_config['mode'] = 'regression'
     ts_config = model_config['ts_config']
 
-    print("TS branch config".format(ts_config))
-
     # Retrieve data and then resize it to fit predefined batch size
-    TS_X_train, TS_Y_train, TS_X_test, TS_Y_test = get_ts_ticker_data(ticker, input_dim=(ts_config['batch_size'],
-                                                                                         ts_config['timesteps'],
-                                                                                         ts_config['features']))
+    TS_X_train, TS_Y_train, TS_X_test, TS_Y_test = \
+        get_ts_ticker_data(ticker, output_shape=
+        (ts_config['batch_size'],ts_config['timesteps'],ts_config['features']),
+                           classification=False)
 
     model.summary()
     print("Inputs: {}".format(model.input_shape))
@@ -164,21 +202,93 @@ def training_model(ticker):
 
     # fit network
     # TODO: Not clear, how to sync up batch sizes across multiple branches
-    history = model.fit(TS_X_train, TS_Y_train, epochs=model_config['epochs'],
-                        batch_size=model_config['ts_config']['batch_size'],
-                        validation_data=(TS_X_test, TS_Y_test), verbose=2, shuffle=False)
 
-    # plot history
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
+    loss = []
+    val_loss = []
+
+
+    for _ in range(model_config['epochs']):
+        history = model.fit(TS_X_train, TS_Y_train, epochs=1, batch_size=model_config['ts_config']['batch_size'],
+                        validation_data=(TS_X_test, TS_Y_test), verbose=2, shuffle=False)
+        loss.append(history.history['loss'])
+        val_loss.append(history.history['val_loss'])
+        model.reset_states()
+
+#
+    # # plot history
+    pyplot.plot(loss, label='train')
+    pyplot.plot(val_loss, label='test')
     pyplot.legend()
     pyplot.interactive(False)
-    pyplot.show()
+    pyplot.savefig(result_folder+"loss_chart.png")
+    pyplot.close()
 
-    print(model_config)
+    return model, model_config
 
+def evaluate_model(model, X, Y, prediction_horizon, result_folder, classification=True):
+
+    if classification:
+        raise NotImplemented("Need to implement validation function for classification mode")
+
+    time_series = list(range(X.shape[0]))
+
+    Y_pred = model.predict(X, batch_size=batch_size)
+
+    pyplot.plot(time_series, Y, label='real')
+    pyplot.plot(time_series, Y_pred, label='predicted')
+    pyplot.legend()
+    pyplot.interactive(False)
+    pyplot.savefig(result_folder+"true_vs_predicted.png")
+    pyplot.close()
+
+
+
+def get_sample_data(ticker, sample_size):
+
+    file = TIMESERIES_FOLDER + "{0}_df.p".format(ticker)
+    with open(file, "rb") as file:
+        ticker_df = pickle.load(file)
+
+    random_idx = random.randint(0, (ticker_df.shape[0] - sample_size - 1))
+
+    X_sample = ticker_df.loc[random_idx:(random_idx + sample_size - 1), ['open', 'high', 'low', 'close', 'volume']]
+    Y_sample = ticker_df.loc[(random_idx + 1):(random_idx+sample_size), ['close']]
+
+    return X_sample, Y_sample
+
+def _prepare_results_folder():
+    result_folder = './results/{0}/'.format(str(uuid.uuid1()))
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+    return result_folder
+
+def _dump_config(result_folder, config):
+    with open(result_folder+'model_config.json', 'w') as outfile:
+        json.dump(config, outfile)
+
+
+batch_size = 64 # TODO: really bad practice. Global variable.
+timesteps = 20
 
 if __name__ == '__main__':
-    ticker = 'FB'
+    result_folder = _prepare_results_folder()
 
-    training_model(ticker)
+    # Runtime configs
+    ticker = 'FB'
+    prediction_horizon = 20 # todo: as of now, this seems to be irrelevant feature.
+
+
+    model, config = training_model(ticker, result_folder)
+
+    TS_X_train, TS_Y_train, TS_X_test, TS_Y_test = \
+        get_ts_ticker_data(ticker, output_shape=(batch_size, timesteps, 5), classification=False)
+
+    evaluate_model(model, X=TS_X_test, Y=TS_Y_test,
+                   classification=False, prediction_horizon=prediction_horizon, result_folder=result_folder)
+
+    config['runtime'] = {'ticker' : ticker, 'prediction_horizon' : prediction_horizon} # adding some runtime parameters to config dump
+    config['model'] = model.to_json() # adding model description to config dump
+    _dump_config(result_folder, config)
+
+    print(result_folder)
+
